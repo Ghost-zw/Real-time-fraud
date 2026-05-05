a0176236b821873fc81765020d0b4fe86d154c85
Tanaka <tanakamutsitu@gmail.com>
2026-05-05 10:22:31 +0200

    fix state files

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "fraud-detection-tf-state-dev"
+    key            = "mvp/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "fraud-terraform-locks"
+    encrypt        = true
+  }
+}
